bfafe38fcc791fa294289076d2d7e24c1b6bd1f8
aaronjfinley <aaronjfinley@gmail.com>
2015-07-27 08:07:37 -0500

    Finished Calculator

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,61 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square_root
+
+    @page_title = "Square Root"
+
+    @num_input = params['num_input'].to_f
+
+    @output = Math.sqrt(@num_input).round(2)
+
+    @output_string = "The square root of #{@num_input} is #{@output}"
+
+    render("calculation_output.html.erb")
+  end
+
+  def square
+
+    @page_title = "Square"
+
+    @num_input = params['num_input'].to_f
+
+    @output = (@num_input**2).round(2)
+
+    @output_string = "The square of #{@num_input} is #{@output}"
+
+    render("calculation_output.html.erb")
+  end
+
+  def random
+
+    @page_title = "Random Number"
+
+    @min = params['min'].to_i
+    @max = params['max'].to_i
+
+    @output = rand(@max - @min + 1) + @min
+
+    @output_string = "A random number between #{@min} and #{@max} is #{@output}"
+
+    render("calculation_output.html.erb")
+  end
+
+  def payment
+
+    @page_title = "Payment"
+
+    @monthly_rate = params['rate'].to_f/10000/12
+    @years = params['years'].to_i
+    @principal = params['principal'].to_f
+
+    @output = @monthly_rate*@principal / (1-(1+@monthly_rate)**-(@years*12))
+
+    @rounded_rate = (params['rate'].to_f/100).round(2)
+
+
+    render("payment_output.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square_root/:num_input", { :controller => "calculations", :action => "square_root" })
+
+  get("/square/:num_input", { :controller => "calculations", :action => "square" })
+
+  get("/random/:min/:max", { :controller => "calculations", :action => "random" })
+
+  get("/payment/:rate/:years/:principal", { :controller => "calculations", :action => "payment" })
+
 end
